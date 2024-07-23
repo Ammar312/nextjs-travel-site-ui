e2a9b7248a157f1dfb681216a35994ed8bda7777
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2024-07-23 13:14:34 +0500

    working on navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,22 @@
+import { NAV_LINKS } from '@/constants'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <nav className='flexBetween max-container padding-container relative z-30 py-5'>
+       <Link href='/'>
+       <Image src='' alt='logo' width={74} height={29}></Image>
+       </Link>
+       <ul className='hidden lg:flex h-full gap-12'>
+        {NAV_LINKS.map((link)=>(
+            <Link href={link.href} key={link.key} className=''>
+                {link.label}
+            </Link>
+        ))}
+       </ul>
+    </nav>
   )
 }
 
