96050a4cff2d09b9be11ae23f430aa87767493e6
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2024-07-27 07:57:21 +0500

    herosection complete

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,8 +11,8 @@ const Footer = () => {
             <Image src="hilink-logo.svg" alt="logo" width={74} height={29} />
           </Link>
           <div className="flex flex-wrap gap-10 sm:justify-between md:flex-1">
-            {FOOTER_LINKS.map((coulmns) => (
-              <FooterCoulmn title={coulmns.title}>
+            {FOOTER_LINKS.map((coulmns, index) => (
+              <FooterCoulmn title={coulmns.title} key={index}>
                 <ul className="regular-14 flex flex-col gap-4 text-gray-30">
                   {coulmns.links.map((link) => (
                     <Link href="/" key={link}>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,26 +7,26 @@ const Hero = () => {
     <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
       <div className="hero-map" />
 
-      <div className="">
+      <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
         <Image
           src="/camp.svg"
           alt="campsvg"
           width={50}
           height={50}
-          className=""
+          className="absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]"
         />
 
-        <h1 className="">Putuk Truno Camp Area</h1>
+        <h1 className="bold-52 lg:bold-88">Putuk Truno Camp Area</h1>
 
-        <p className="">
+        <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
           {" "}
           We want to be on each of your journeys seeking the satisfaction of
           seeing the incorruptible beauty of nature. We can help you on an
           adventure around the world in just one app
         </p>
 
-        <div>
-          <div>
+        <div className="my-11 flex flex-wrap gap-5">
+          <div className="flex items-center gap-2">
             {Array(5)
               .fill(1)
               .map((index) => (
@@ -39,12 +39,15 @@ const Hero = () => {
                 />
               ))}
           </div>
-          <p>
-            198k<span>Excellent Reviews</span>
+          <p className="bold-16 lg:bold-20 text-blue-70">
+            198k
+            <span className="regular-16 lg:regular-20 ml-1">
+              Excellent Reviews
+            </span>
           </p>
         </div>
 
-        <div>
+        <div className="flex flex-col sm:flex-row w-full gap-3">
           <Button type="button" title="Download App" variant="btn_green" />
           <Button
             type="button"
@@ -55,24 +58,24 @@ const Hero = () => {
         </div>
       </div>
 
-      <div>
-        <div>
-          <div>
-            <div>
-              <p>Distance</p>
+      <div className="relative flex flex-1 items-start">
+        <div className="relative z-20 flex flex-col w-[268px] gap-8 rounded-3xl bg-green-90 px-7 py-8">
+          <div className="flex flex-col">
+            <div className="flexBetween">
+              <p className="regular-16 text-gray-20">Location</p>
               <Image src="/close.svg" alt="close" width={24} height={24} />
             </div>
-            <p>Aguas Calientes</p>
+            <p className="bold-20 text-white">Aguas Calientes</p>
           </div>
 
-          <div>
-            <div>
-              <p>Distance</p>
-              <p>173.28 mi</p>
+          <div className="flexBetween">
+            <div className="flex flex-col">
+              <p className="regular-16 block text-gray-20">Distance</p>
+              <p className="bold-20 text-white">173.28 mi</p>
             </div>
-            <div>
-              <p>Elevation</p>
-              <p>2.040 km</p>
+            <div className="flex flex-col">
+              <p className="regular-16 block text-gray-20">Elevation</p>
+              <p className="bold-20 text-white">2.040 km</p>
             </div>
           </div>
         </div>
