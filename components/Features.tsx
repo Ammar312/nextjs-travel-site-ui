c6692feb01355009408f91c4d629d7b55e3f542b
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2024-07-29 08:28:59 +0500

    feature component completed

--- components/Features.tsx
+++ components/Features.tsx
@@ -4,9 +4,9 @@ import React from "react";
 
 const Features = () => {
   return (
-    <section>
-      <div>
-        <div>
+    <section className="flexCenter flex-col overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">
+      <div className="max-container padding-container w-full relative flex justify-end">
+        <div className="flex flex-1 lg:min-h-[900px]">
           <Image
             src="/phone.png"
             alt="phone"
@@ -15,8 +15,8 @@ const Features = () => {
             className="feature-phone"
           />
         </div>
-        <div>
-          <div>
+        <div className="z-20 w-full flex flex-col lg:w-[60%]">
+          <div className="relative">
             <Image
               src="/camp.svg"
               alt="camp"
@@ -26,7 +26,7 @@ const Features = () => {
             />
             <h2 className="bold-40 lg:bold-64">Our Features</h2>
           </div>
-          <ul>
+          <ul className="mt-10 grid gap-10 md:grid-cols-2 lg:mg-20 lg-gap-20">
             {FEATURES.map((feature) => (
               <FeatureItem
                 title={feature.title}
@@ -50,12 +50,14 @@ type FeatureItem = {
 
 const FeatureItem = ({ title, icon, description }: FeatureItem) => {
   return (
-    <li>
-      <div>
+    <li className="w-full flex flex-1 flex-col items-start">
+      <div className="rounded-full p-4 lg:p-7 bg-green-50">
         <Image src={icon} alt="map" width={28} height={28} />
       </div>
-      <h2>{title}</h2>
-      <p>{description}</p>
+      <h2 className="boid-20 lg:bold-32 mt-5 capitalize">{title}</h2>
+      <p className="regular-16 mt-5 bg-white/80 text-gray-30 lg:mt-[30px] lg:bg-none">
+        {description}
+      </p>
     </li>
   );
 };
