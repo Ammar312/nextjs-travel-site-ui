9b313009c7b412f17bc7a0b57aa207d694a80b1a
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2024-07-29 06:51:24 +0500

    camp component complete

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -15,23 +15,25 @@ const CampSite = ({
   peopleJoined,
 }: CampProps) => {
   return (
-    <div>
-      <div>
-        <div>
-          <div>
+    <div
+      className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}
+    >
+      <div className="flex flex-col items-start justify-between h-full p-6 lg:px-20 lg:py-10">
+        <div className="flexCenter gap-4">
+          <div className="rounded-full bg-green-50 p-4">
             <Image src="/folded-map.svg" alt="camp" width={28} height={28} />
           </div>
-          <div>
-            <h4>{title}</h4>
-            <p>{subtitle}</p>
+          <div className="flex flex-col gap-1">
+            <h4 className="bold-18 text-white">{title}</h4>
+            <p className="regular-14 text-white">{subtitle}</p>
           </div>
         </div>
-        <div>
-          <span>
+        <div className="flexCenter gap-6">
+          <span className="flex -space-x-4 overflow-hidden">
             {PEOPLE_URL.map((url) => (
               <Image
                 src={url}
-                alt=""
+                alt="person"
                 key={url}
                 width={52}
                 height={52}
@@ -39,7 +41,7 @@ const CampSite = ({
               />
             ))}
           </span>
-          <p>{peopleJoined}</p>
+          <p className="bold-16 md:bold-20 text-white">{peopleJoined}</p>
         </div>
       </div>
     </div>
@@ -48,10 +50,10 @@ const CampSite = ({
 
 const Camp = () => {
   return (
-    <section>
-      <div>
+    <section className="2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
+      <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
         <CampSite
-          backgroundImage="bg-bg-img1"
+          backgroundImage="bg-bg-img-1"
           title="Putuk Truno Camp"
           subtitle="Prigen, Pasuruan"
           peopleJoined="50+ Joined"
@@ -64,12 +66,12 @@ const Camp = () => {
         />
       </div>
 
-      <div>
-        <div>
-          <h2>
+      <div className="flexEnd mt-10 px-6 lg:-mt-60 lg:mr-6">
+        <div className="bg-green-50 p-6 lg:max-w-[500px] xl:max-w-[734px] xl:rounded-5xl xl:px-16 xl:py-20 relative w-full overflow-hidden rounded-3xl">
+          <h2 className="regular-24 md:regular-32 2xl:regular-64 capitalize text-white">
             <strong>Feeling Lost</strong> And Not Knowing The Way?
           </h2>
-          <p>
+          <p className="regular-14 xl:regular-16 mt-5 text-white">
             {" "}
             Starting from the anxiety of the climbers when visiting a new
             climbing location, the possibility of getting lost is very large.
