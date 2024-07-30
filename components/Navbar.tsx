064c268690d46ce7b63f4daa5a82c3a23d9cad34
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2024-07-30 15:26:31 +0500

    complete

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,6 +10,7 @@ const Navbar = () => {
   const [isDrawerOpen, setisDrawerOpen] = useState(false);
   const toggleDrawer = () => {
     setisDrawerOpen(!isDrawerOpen);
+    console.log("toggle");
   };
   return (
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
@@ -44,29 +45,46 @@ const Navbar = () => {
         onClick={toggleDrawer}
       />
       {isDrawerOpen && (
-        <div className="fixed top-0 right-0 h-full w-64 bg-white shadow-lg z-40 p-5">
-          <Image
-            src="/close.svg"
-            alt="close"
-            width={24}
-            height={24}
-            onClick={toggleDrawer}
-            color="#000000"
-          />
-          {/* <button onClick={toggleDrawer} className="mb-5">
-            Close
-          </button> */}
-          <ul className="flex flex-col gap-4">
-            {NAV_LINKS.map((link) => (
-              <Link
-                href={link.href}
-                key={link.key}
-                className="regular-24 text-gray-50  cursor-pointer pb-1.5 transition-all hover:font-bold"
-              >
-                {link.label}
-              </Link>
-            ))}
-          </ul>
+        <div className="fixed top-0 right-0 h-full w-64 md:w-80 lg:hidden bg-white shadow-lg z-40 p-5  border-green-50">
+          <div className="flexBetween mt-3 mb-7">
+            <Image src="/camp.svg" alt="campsvg" width={50} height={50} />
+            <Image
+              src="/cancel.png"
+              alt="close"
+              width={35}
+              height={35}
+              onClick={toggleDrawer}
+            />
+          </div>
+          <div className="flex flex-col  h-full">
+            <ul className="flex flex-col gap-4 mb-32">
+              {NAV_LINKS.map((link) => (
+                <Link
+                  href={link.href}
+                  key={link.key}
+                  className="regular-24 text-gray-50  cursor-pointer pb-1.5 transition-all hover:font-bold"
+                >
+                  {link.label}
+                </Link>
+              ))}
+            </ul>
+            <div className="flex flex-col gap-5 ">
+              <Button
+                type="button"
+                title="App Store"
+                icon="/apple.svg"
+                variant="border-green-50 bg-white px-8 py-3 text-green-50"
+                full
+              />
+              <Button
+                type="button"
+                title="Play Store"
+                icon="/android.svg"
+                variant="btn_dark_green_outline"
+                full
+              />
+            </div>
+          </div>
         </div>
       )}
     </nav>
