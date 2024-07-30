2f350b3ac8e79b47c5f7cb896cbda9318388b868
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2024-07-30 07:16:14 +0500

    drawer add styling remaining

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,16 @@
+"use client";
+
 import { NAV_LINKS } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import Button from "./Button";
 
 const Navbar = () => {
+  const [isDrawerOpen, setisDrawerOpen] = useState(false);
+  const toggleDrawer = () => {
+    setisDrawerOpen(!isDrawerOpen);
+  };
   return (
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
@@ -35,7 +41,34 @@ const Navbar = () => {
         width={32}
         height={32}
         className="inline-block cursor-pointer lg:hidden"
+        onClick={toggleDrawer}
       />
+      {isDrawerOpen && (
+        <div className="fixed top-0 right-0 h-full w-64 bg-white shadow-lg z-40 p-5">
+          <Image
+            src="/close.svg"
+            alt="close"
+            width={24}
+            height={24}
+            onClick={toggleDrawer}
+            color="#000000"
+          />
+          {/* <button onClick={toggleDrawer} className="mb-5">
+            Close
+          </button> */}
+          <ul className="flex flex-col gap-4">
+            {NAV_LINKS.map((link) => (
+              <Link
+                href={link.href}
+                key={link.key}
+                className="regular-24 text-gray-50  cursor-pointer pb-1.5 transition-all hover:font-bold"
+              >
+                {link.label}
+              </Link>
+            ))}
+          </ul>
+        </div>
+      )}
     </nav>
   );
 };
